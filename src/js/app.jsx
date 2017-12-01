fab8061dd283cd1fb9b5c53bc39215c6e83fca0e
Corey Cacananta <cacananta@gmail.com>
2017-12-01 14:41:30 -0800

    added bootstrap

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -31,25 +31,39 @@ export default class App extends React.Component {
     this.setState(Object.assign(this.state, { output: this.calculatePayment(this.state.balance, this.state.rate, this.state.term, 12).toFixed(2) }));
     event.preventDefault();
   }
-  
+
   render() {
     return (
-      <div className="container">
-        <h3>Mortgage Calculator</h3>
-        <h3>Loan Balance</h3>
-        <input name="balance" type="number" onChange={this.handleChange} />
-        {/* your JSX goes here */}
-        <h3>Interest Rate (%)</h3>
-        <input placeholder="Ex: 6.45%" name="rate" type="number" step="0.01" onChange={this.handleChange} />
-        <h3>Loan Terms (years)</h3>
-        <select name="term" onChange={this.handleChange}>
-          <option>Select Term</option>
-          <option>30</option>
-          <option>15</option>
-        </select>
-        <button name="submit" onClick={this.handleSubmit}>Calculate Mortgage Payment</button>
-        <div id="output"><h3>${[this.state.output]} is your payment</h3></div>
-      </div>
+      <div>
+        <div className="container text-center">
+          <div className="row my-3">
+            <div className="col-lg-4">
+              <h3>Loan Balance</h3>
+              <input name="balance" type="number" onChange={this.handleChange} />
+            </div>
+            <div className="col-lg-4">
+              <h3>Interest Rate (%)</h3>
+              <input placeholder="Ex: 6.45%" name="rate" type="number" step="0.01" onChange={this.handleChange} />
+            </div>
+            <div className="col-lg-4">
+              <h3>Loan Terms (years)</h3>
+              <div className="dropdown">
+                <select name="term" onChange={this.handleChange}>
+                  <option className="dropdown-item">Select Term</option>
+                  <option className="dropdown-item">30</option>
+                  <option className="dropdown-item">15</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button name="submit" onClick={this.handleSubmit} className="btn btn-info">Calculate Mortgage Payment</button>
+          </div>
+        </div>
+
+
+        {!this.state.output ? <div></div> : <div id="output"><h3>${this.state.output} is your payment</h3></div>}
+      </div >
     );
   }
 }
